6acc0d42389e594a4cee18b042ab43b56013dfa4
Wilyan <92753649+wilyanmatos@users.noreply.github.com>
2022-09-15 16:20:37 -0300

    Laço de repetição

--- Projetos-Java/Hello-World/src/Laços_de_Repetição.java
+++ Projetos-Java/Hello-World/src/Laços_de_Repetição.java
@@ -1,2 +1,11 @@
 public class Laços_de_Repetição {
+    public static void main(String[] args){
+        int num = 0;
+        while(num != 10){
+            num += 1;
+            System.out.println(num);
+
+        }
+
+    }
 }
