5b06bc81ebbfa1712ac4a9b57b6451d7e1780af7
Wilyan <92753649+wilyanmatos@users.noreply.github.com>
2022-09-16 09:18:15 -0300

    Laço de Repetição

--- Projetos-Java/Hello-World/src/Laços_de_Repetição.java
+++ Projetos-Java/Hello-World/src/Laços_de_Repetição.java
@@ -1,11 +1,11 @@
 public class Laços_de_Repetição {
     public static void main(String[] args){
-        int num = 0;
-        while(num != 10){
-            num += 1;
-            System.out.println(num);
+        // Variavel i receber 1, precisando ser menor ou igual a 10, caso não for adicione mais 1
 
+        for (int i = 1; i <= 10; i += 1){
+            int num = 1;
+            int valor = i * (num + 1);
+            System.out.printf("%s x %s = %s%n", num + 1, i, valor);
         }
-
     }
 }
